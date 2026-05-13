c0add3e0238c05888cca774a0bcc4bbcfa76cf72
Oliver Edman Chrantz <o.edman@icloud.com>
2026-05-14 01:15:32 +0200

    Refactor init.lua for improved plugin setup

--- init.lua
+++ init.lua
@@ -1,35 +1,48 @@
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.opt.number = true           -- Visa radnummer
-vim.opt.relativenumber = true   -- Relativa nummer (bra för snabb navigation)
-vim.opt.termguicolors = true    -- Krävs för snygga teman
-vim.opt.cursorline = true       -- Markera raden du står på
 
-require("lazy").setup({  -- Start på plugin-listan
-  { 
+vim.opt.termguicolors = true
+vim.opt.number = true
+vim.opt.cursorline = true
+
+
+require("lazy").setup({
+
+  {
     "folke/tokyonight.nvim",
-    lazy = false, 
-    priority = 1000, 
+    lazy = false,
+    priority = 1000,
     config = function()
-      vim.cmd([[colorscheme tokyonight-storm]])
+      vim.cmd([[colorscheme tokyonight-night]])
+      vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
     end,
   },
-  
+
+
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function() require('lualine').setup() end
+  },
+
+ 
   {
     "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("nvim-tree").setup()
+      require("nvim-tree").setup({
+        view = { width = 30, side = "left" },
+      })
     end,
   },
-}) 
+})
+
+vim.keymap.set('n', 'n', function()
+  local nvimtree = require("nvim-tree.api")
+  nvimtree.tree.toggle()
+end, { desc = "Toggle NvimTree" })
